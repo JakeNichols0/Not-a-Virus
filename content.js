55381886445e3c2cc8d49b229a7164b6cd85fe1f
JakeNichols0 <161376788+JakeNichols0@users.noreply.github.com>
2025-05-09 18:31:32 -0500

    Direction it spins is randomized

--- content.js
+++ content.js
@@ -6,7 +6,21 @@ spin(d);
 
 function spin(e) {
     for (let i=0; i<e.length; i++) {
-        e[i].style.animation = `spin ${Math.floor(Math.random()*51)+10}s linear 0s infinite`;
+        let direct = "";
+        switch(Math.floor(Math.random() * 4)) {
+            case 0:
+                direct = "normal";
+                break;
+            case 1:
+                direct = "reverse";
+                break;
+            case 2:
+                direct = "alternate";
+                break;
+            case 3:
+                direct = "alternate-reverse";
+        }
+        e[i].style.animation = `spin ${Math.floor(Math.random()*51)+10}s linear 0s infinite ${direct}`;
         spin(e[i].children);
     }
 }
